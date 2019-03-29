1fe2cff54a4285da5cefa79e6ec8c1555590500b
Saad Talaat <saadtalaat@gmail.com>
2019-03-29 12:36:10 +0200

    [examples] modifications to pubsub example, nicer logging, less code, timestamping

--- motiv/examples/pubsub.py
+++ motiv/examples/pubsub.py
@@ -1,15 +1,29 @@
 import time
+import logging
 
-from motiv.actor.process import ZMQActor
-from motiv.streams.zmq import ZMQEmitter, ZMQSubscriber
+from motiv.actor.process import Actor
+from motiv.streams import Emitter, Subscriber
 
 
-publisher = ZMQEmitter(address="/tmp/publisher", scheme="ipc")
-subscriber = ZMQSubscriber(address="/tmp/publisher", scheme="ipc")
+publisher = Emitter(address="/tmp/publisher", scheme="ipc")
+subscriber = Subscriber(address="/tmp/publisher", scheme="ipc")
 
-class MyPublisher(ZMQActor):
+def getLogger(name):
+    logger = logging.getLogger(f"motiv:{name}")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(name)-12s: %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+class MyPublisher(Actor):
 
     def preStart(self):
+        self.logger = getLogger(self.name)
         self.stream_out.connect()
 
     def postStop(self):
@@ -17,11 +31,15 @@ class MyPublisher(ZMQActor):
 
     def tick(self):
         time.sleep(2)
+        ts = int(time.monotonic() * 1e6)
         self.publish(1, b"Hello world")
+        print("\n")
+        self.logger.info("\tPublishing to subscribers\t(monotonic (us): {:,})".format(ts))
 
-class MySubscriber(ZMQActor):
+class MySubscriber(Actor):
 
     def preStart(self):
+        self.logger = getLogger(self.name)
         subscriber.subscribe(1)
         self.stream_in.connect()
 
@@ -30,12 +48,15 @@ class MySubscriber(ZMQActor):
 
     def tick(self):
         channel, payload = self.receive()
-        print(f"{self.name} Subscriber received {payload}")
+        ts = int(time.monotonic() * 1e6)
+        self.logger.info("\tReceived {}\t\t(monotonic (us): {:,})".format(payload, ts))
 
 
-pub = MyPublisher("pub1")  + publisher
-sub = MySubscriber("sub1")  + subscriber
-sub2 = MySubscriber("sub2") + subscriber
-pub.start()
-sub.start()
-sub2.start()
+if __name__ == '__main__':
+    make_subscriber = lambda idx: (MySubscriber(f"subscriber[{idx}]") + subscriber)
+
+    pub = MyPublisher("publisher[1]")  + publisher
+    subscribers = [make_subscriber(i) for i in range(5)]
+
+    pub.start()
+    [sub.start() for sub in subscribers]
